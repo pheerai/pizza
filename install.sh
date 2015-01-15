3083b8255de6a580ac8eb70b3667e269293381ab
Oliver Rümpelein <oli_r@fg4f.de>
2015-01-15 17:22:32 +0100

    Added one PREFIX variable to install.sh
    still, PREFIX is applied to both the binary as well as the include path.
    This should be changed in a future release (see issue #7 )

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #/bin/bash
+# PREFIX gets set with the following priorities:
+# 1) use provided argument
+# 2) use environement PREFIX
+# 3) use "/usr/local/"
 
-install -m 755 src/pizza /usr/local/bin/pizza
-mkdir -p /usr/local/share/pizza
-install -m 644 assets/* /usr/local/share/pizza/
+
+if [ "$1" ]; then
+	PREFIX:="$1"
+else
+	: ${PREFIX:="/usr/local"}
+fi
+
+# Create necessary directories
+
+for i in "${PREFIX}/bin" "${PREFIX}/share/pizza"; do
+	if [ ! -d "$i" ]; then
+		mkdir -p "$i"
+	fi
+done
+
+install -m 755 src/pizza "${PREFIX}/bin/pizza"
+install -m 644 assets/* "${PREFIX}/share/pizza/"
+
+# Tell the user what to do next, if he used another PREFIX
+
+if [ "${PREFIX}" != "/usr/local" ]; then
+	echo 'Your installation path differs from /usr/local'
+	echo 'For pizza to run properly, please either modify the script'
+	echo 'or set the PIZZAINC-variable accordingly.'
+	echo '(Add the following to your ${shell}rc:'
+	echo -e "\t export PIZZAINC=\"${PREFIX}/share/pizza/\""
+	echo ")"
+fi
